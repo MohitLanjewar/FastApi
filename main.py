ca5e886e06fed63b4d2edea0886e9910c9573197
Mohit Lanjewar <mohitgll001@gmail.com>
2025-02-26 01:10:16 +0530

    test

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content : str
+    published : bool =True
+    rating : Optional[int] = None
 
 @app.get("/posts")
 def getPost():
     return {"message": "this is your post"}
 
 @app.post("/predict")
-def postreq(payload: dict= Body(...)):
-    print(payload)
-    return {"message": payload}
+def postreq(post: Post):
+    print(post)
+    print(post.model_dump())
+    return {"message": post}
